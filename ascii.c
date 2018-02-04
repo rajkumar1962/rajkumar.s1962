c023737f6ee77903be275d0390338ee1943653db
rajkumar1962 <35728415+rajkumar1962@users.noreply.github.com>
2018-02-04 14:57:01 +0530

    Create ascii.c

---
+++ ascii.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    char c;
+    printf("enter the value of c");
+    scanf("%c",&c);
+    ascii(c);
+}
+void ascii(c);
+printf("the ASCII value is :%d",c);
+
+    
+}
