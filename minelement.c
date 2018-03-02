5383ef37588f3a697dfff564e472d9512513a275
rajkumar1962 <35728415+rajkumar1962@users.noreply.github.com>
2018-03-02 14:29:32 +0530

    Create minelement.c

---
+++ minelement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[15],i,b;
+printf("enter the value of b:");
+scanf("%d",&b);
+for(i=0;i<b;i++)
+{
+scanf("%d",a[i]);
+}
+for(i=0;i<b;i++)
+{
+printf("minmum element is");
+
+}
