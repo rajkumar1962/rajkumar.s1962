546c1bbd872df1e43495b9eb6a9a75e528033f66
rajkumar1962 <35728415+rajkumar1962@users.noreply.github.com>
2018-03-15 07:07:52 -0400

    Create s28.c

---
+++ s28.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[30],i,j,n;
+printf("enter the array");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+printf("enter the array");
+}
+for(j=0;j<=n;j++)
+{
+printf("%d",a[i]);
+}
+}
